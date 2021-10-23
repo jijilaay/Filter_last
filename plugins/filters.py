6057a09653ade71fb4b8e4cfd64ae82d874a4bd6
vihangapahasara <92439499+vihangapahasara@users.noreply.github.com>
2021-10-23 16:25:51 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -62,7 +62,7 @@ async def filter(client: Bot, message: Message):
                 [InlineKeyboardButton(text="📃 Pages 1/1",callback_data="pages")]
             )
             await message.reply_text(
-                f"<b>😊 single developer @viha_is_Power ☺මෙන්න ඔයා හොයන අහවල්, {message.text}</b>",
+                f"<b>😊 single developer vihanga from Rule Breakers ☺මෙන්න ඔයා හොයන අහවල්, {message.text}</b>",
                 reply_markup=InlineKeyboardMarkup(buttons)
             )
             return
@@ -78,7 +78,7 @@ async def filter(client: Bot, message: Message):
         )
 
         await message.reply_text(
-                f"<b> 😊 single developer @viha_is_Power ☺මෙන්න ඔයා හොයන අහවල්{message.text}</b>",
+                f"<b> 😊 single developer vihanga from Rule Breakers ☺මෙන්න ඔයා හොයන අහවල් {message.text}</b>",
                 reply_markup=InlineKeyboardMarkup(buttons)
             )    
 
