a94d7e73744081d5ff5979b83c897df64b700840
vihangapahasara <92439499+vihangapahasara@users.noreply.github.com>
2021-10-29 13:33:27 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -125,7 +125,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 buttons.append(
                     [InlineKeyboardButton(f"📃 Pages {int(index)+2}/{data['total']}", callback_data="pages"),InlineKeyboardButton("🎬 𝕊𝕙𝕒𝕣𝕖 𝕆𝕦𝕣 𝔾𝕣𝕠𝕦𝕡", url="https://t.me/share/url?url=https://t.me/joinchat/Q1uroGQ645U1OTg1&text=⚡️සුපිරි%20Movie%20Group%20එකක්%20තියනව.%20🎬%20Film%20එකේ%20නම%20දැම්ම%20ගමන්%20Film%20එක%20දෙන්නව😳.%20ඔන්න%20Link%20එක%20ඉක්මනට%20Join%20වෙන්න✅")]
                 )
-                buttons.append(
+
 
                     
                 await query.edit_message_reply_markup( 
